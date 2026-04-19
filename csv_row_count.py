6a928ab518eccfeea0f4b624cd37113f45c7ae0a
Aastha-Singh-Baghel <aasthasinghbaghel082@gmail.com>
2026-04-19 12:31:06 +0530

    csv_row_count.py

--- csv_row_count.py
+++ csv_row_count.py
@@ -1,20 +1,12 @@
-import csv
-
 # Take filename from user
 filename = input("Enter CSV file name: ")
 
 try:
     with open(filename, 'r') as file:
-        reader = csv.reader(file)
-        
-        row_count = 0
-        
-        for row in reader:
-            row_count += 1
-        
-        print("Total number of rows:", row_count)
+        lines = file.readlines()
+        print("Total number of rows:", len(lines))
 
 except FileNotFoundError:
-    print("File not found. Please check the file name.")
+    print("File not found.")
 except PermissionError:
-    print("Permission denied to read the file.")
+    print("Permission denied.")
